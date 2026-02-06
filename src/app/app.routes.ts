ad0f8e53c9ff60951c59ef9e3d7cbf0db48154ca
Daniel Mommsen <danielmommsen@hotmail.com>
2026-02-06 10:44:03 +0200

    Add user todos route with local add-todo form and store-backed state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './features/users/users-list.component';
+import { UserTodosComponent } from './features/user-todos/user.todos.component';
 
 export const routes: Routes = [
      { path: '', pathMatch: 'full', redirectTo: 'users' },
      { path: 'users', component: UsersListComponent},
-
+     { path: 'users/:id', component: UserTodosComponent },
      { path: '**', redirectTo: 'users' },
 ];
